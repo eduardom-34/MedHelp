ad38fa435f0709f4ffde7417e4f77c1b07afb5d8
edu Kubuntu <eduardomelchor34@gmail.com>
2024-09-06 11:16:21 -0600

    Basic Routing for specialties pages

--- MedHelpAngular/src/app/specialties/specialties-routing.module.ts
+++ MedHelpAngular/src/app/specialties/specialties-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { SpecialistPageComponent } from './pages/specialist-page/specialist-page.component';
 
 const routes: Routes = [
   {
@@ -11,6 +14,26 @@ const routes: Routes = [
       {
         path: 'new-specialist',
         component: NewPageComponent
+      },
+      {
+        path: 'search',
+        component: SearchPageComponent
+      },
+      {
+        path: 'edit/:id',
+        component: NewPageComponent
+      },
+      {
+        path: 'list',
+        component: ListPageComponent
+      },
+      {
+        path: ':id',
+        component: SpecialistPageComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'list'
       }
     ]
   }
