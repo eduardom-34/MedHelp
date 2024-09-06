cb7ebad9cf878667010162abd1fe2a9c44b3c1aa
edu Kubuntu <eduardomelchor34@gmail.com>
2024-09-06 17:56:13 -0600

    Login and Sign up pages

--- MedHelpAngular/src/app/auth/auth.module.ts
+++ MedHelpAngular/src/app/auth/auth.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { MaterialModule } from '../material/material.module';
 
-import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 
 @NgModule({
--- MedHelpAngular/src/app/material/material.module.ts
+++ MedHelpAngular/src/app/material/material.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
+
 import { MatButtonModule } from '@angular/material/button'
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+
 
 
 
 @NgModule({
   exports:[
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }
