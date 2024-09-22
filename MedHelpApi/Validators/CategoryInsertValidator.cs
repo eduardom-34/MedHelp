3dc585e2f2ea629d55c61d4a4199ebe855ca5abe
edu Kubuntu <eduardomelchor34@gmail.com>
2024-09-22 14:50:44 -0600

    Show my own error messages with fluent validation

--- MedHelpApi/Validators/CategoryInsertValidator.cs
+++ MedHelpApi/Validators/CategoryInsertValidator.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Data;
 using FluentValidation;
 using MedHelpApi.DTOs;
 
@@ -8,6 +9,8 @@ public class CategoryInsertValidator : AbstractValidator<CategoryInsertDto>
 {
   public CategoryInsertValidator()
   {
-    RuleFor(c => c.Name).NotEmpty();
+    RuleFor(c => c.Name).NotEmpty().WithMessage("The name is required");
+    RuleFor(c => c.Name).Length(2, 200).WithMessage("The names must have 2 to 200 chars");
+    RuleFor(c => c.Description).NotEmpty().WithMessage("Description cannot be empty");
   }
 }
