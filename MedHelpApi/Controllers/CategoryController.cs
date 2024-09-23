cbf1dd8eda93091f007f445277f68f7369e206ba
edu Kubuntu <eduardomelchor34@gmail.com>
2024-09-23 17:49:27 -0600

    Using services in our controllers for logic, httpGet

--- MedHelpApi/Controllers/CategoryController.cs
+++ MedHelpApi/Controllers/CategoryController.cs
@@ -32,30 +32,14 @@ namespace MedHelpApi.Controllers
 
         [HttpGet]
         public async Task<IEnumerable<CategoryDto>> Get() =>
-            await _context.Categories.Select( c => new CategoryDto{
-                Id = c.CategoryID,
-                Name = c.Name,
-                Description = c.Description
-            }).ToListAsync();
+            await _categoryService.Get();
 
         [HttpGet("{id}")]
         public async Task<ActionResult<CategoryDto>> GetById(int id)
         {
-            var category = await _context.Categories.FindAsync(id);
+            var categoryDto = await _categoryService.GetById(id);
 
-            if(category == null)
-            {
-                return NotFound();
-            }
-
-            var categoryDto = new CategoryDto
-            {
-                Id = category.CategoryID,
-                Name = category.Name,
-                Description = category.Description
-            };
-
-            return Ok(categoryDto);
+            return categoryDto == null ? NotFound() : Ok(categoryDto);
         }
 
         [HttpPost]
--- MedHelpApi/Services/CategoryService.cs
+++ MedHelpApi/Services/CategoryService.cs
@@ -1,31 +1,55 @@
 using System;
 using MedHelpApi.DTOs;
+using MedHelpApi.Models;
 using MedHelpApi.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace MedHelpApi.Services;
 
 public class CategoryService : ICategoryService
 {
-    public Task<IEnumerable<CategoryDto>> Get()
+    private MedHelpContext _context;
+
+    public CategoryService(MedHelpContext context)
+    {
+        _context = context;
+    }
+    public async Task<IEnumerable<CategoryDto>> Get() =>
+        await _context.Categories.Select( c => new CategoryDto{
+                Id = c.CategoryID,
+                Name = c.Name,
+                Description = c.Description
+            }).ToListAsync();
+
+    public async Task<CategoryDto> GetById(int id)
+    {
+        var category = await _context.Categories.FindAsync(id);
+
+            if(category != null)
+            {
+                var categoryDto = new CategoryDto
+                {
+                    Id = category.CategoryID,
+                    Name = category.Name,
+                    Description = category.Description
+                };
+
+                return categoryDto;
+            }
+            
+            return null;
+    }
+    public async Task<CategoryDto> Add(CategoryInsertDto categoryInsertDto)
     {
         throw new NotImplementedException();
     }
 
-    public Task<CategoryDto> GetById(int id)
-    {
-        throw new NotImplementedException();
-    }
-    public Task<CategoryDto> Add(CategoryInsertDto categoryInsertDto)
+    public async Task<CategoryDto> Update(int id, CategoryInsertDto categoryInsertDto)
     {
         throw new NotImplementedException();
     }
 
-    public Task<CategoryDto> Update(int id, CategoryInsertDto categoryInsertDto)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<CategoryDto> Delete(int id)
+    public async Task<CategoryDto> Delete(int id)
     {
         throw new NotImplementedException();
     }
