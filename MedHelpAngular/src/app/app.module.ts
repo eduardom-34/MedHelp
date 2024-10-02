9a8ff2c1943cddec9f010079f0fa3fd965c348d3
CesarLinux <eduardomelchor34@gmail.com>
2024-10-02 10:11:30 -0600

    Angualr back to started

--- MedHelpAngular/src/app/app.module.ts
+++ MedHelpAngular/src/app/app.module.ts
@@ -1,21 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [
-    provideAnimationsAsync(),
-    provideHttpClient(),
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
