dcfcb87230c88fec2b340a2c532906d42c348593
edu Kubuntu <eduardomelchor34@gmail.com>
2024-11-25 19:50:10 -0600

    Autocomplete component, still we need to fix some issues

--- MedHelpAngular/src/app/medhelp/components/autocomplete-specialty/autocomplete-specialty.component.ts
+++ MedHelpAngular/src/app/medhelp/components/autocomplete-specialty/autocomplete-specialty.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Specialty } from '../../interfaces/specialty.interface';
+import { FormControl } from '@angular/forms';
+import { map, Observable, startWith } from 'rxjs';
 
 @Component({
-  selector: 'app-autocomplete-specialty',
+  selector: 'autocomplete-specialty',
   templateUrl: './autocomplete-specialty.component.html',
   styleUrl: './autocomplete-specialty.component.css'
 })
-export class AutocompleteSpecialtyComponent {
+export class AutocompleteSpecialtyComponent implements OnInit{
+
+  myControl = new FormControl<string | Specialty>('');
+
+  options: Specialty[] = [];
+  specialties: Specialty[] = [];
+  filteredOptions: Observable<Specialty[]>;
+
+  ngOnInit(): void {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => {
+        const name = typeof value === 'string' ? value : value.name;
+        return name ? this._filter(name as string) : this.options.slice();
+      })
+    )
+  }
+
+  displayFn(user: Specialty): string {
+    return user && user.name ? user.name : '';
+  }
+
+  private _filter(specialtyName: string): Specialty[] {
+    const filterValue = specialtyName.toLowerCase();
+
+    return this.options.filter(option => option.name.toLowerCase().includes(filterValue));
+  }
+
 
 }
